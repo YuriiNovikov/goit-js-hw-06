943918aced5a8868606316f09cdb9aa9e34a338f
YuriiNovikov <zysnovikov@gmail.com>
2023-01-29 19:09:51 +0200

    task-1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,1 +1,8 @@
-console.log(length.#categories)
+const listEl = document.querySelector('#categories');
+console.log('Number of categories:', listEl.childElementCount);
+const items = [...listEl.children];
+items.forEach((element) => {
+    console.log('Category:', element.firstElementChild.textContent);
+    console.log('Elements:', element.lastElementChild.childElementCount);
+  });
+
