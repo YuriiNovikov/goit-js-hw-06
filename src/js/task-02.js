7de0485b0bec05339531b1dd6cc6a90593c37d85
YuriiNovikov <zysnovikov@gmail.com>
2023-01-31 13:17:25 +0200

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+
+const makeIngredientsList = option => {
+  return option.map(option => {
+    const liEl = document.createElement('li');
+    liEl.classList.add('item');
+    liEl.textContent = option;
+
+    return liEl;
+  });
+};
+
+const elements = makeIngredientsList(ingredients);
+console.log(elements);
+listEl.append(...elements);
