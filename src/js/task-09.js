ab7b3bcf20a70c8e08d8efb0f15397da819859bd
YuriiNovikov <zysnovikov@gmail.com>
2023-02-08 20:13:22 +0200

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const nameColor = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
+btnChangeColor.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  nameColor.textContent = body.style.backgroundColor;
+});
